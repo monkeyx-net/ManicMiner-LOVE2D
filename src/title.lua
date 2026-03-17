0dd5f4f989a2d65b2ef4d8ac1234beb8f94c5cd2
monkeyx-net <tim@monkeyx.net>
2026-03-17 12:13:40 +0000

    back and start to quit

--- src/title.lua
+++ src/title.lua
@@ -99,7 +99,7 @@ local TEXT_32 = "                                "
 local textTicker = {
     "\x01\x00\x02\x02" .. "M" .. "\x02\x06" .. "A" .. "\x02\x04" .. "N" .. "\x02\x05" .. "I" .. "\x02\x03" .. "C " .. "\x02\x05" .. "M" .. "\x02\x03" .. "I" .. "\x02\x02" .. "N" .. "\x02\x06" .. "E" .. "\x02\x04" .. "R   " ..
     "\x02\x07" .. "(C) Bug-Byte Ltd. 1983   By Matthew Smith" .. TEXT_32 ..
-    "\x02\x05" .. "Cursor Keys/PAD = Left & Right   " .. "\x02\x06" .. "Space/A Button = Jump   " .. "\x02\x03" .. "Pause/Tab/X Button = Pause      " .. "\x02\x04" .. "Alt/Y Button = Tune On/Off     " .. "\x02\x05" .. "S/B Button = High Scores" .. TEXT_32 ..
+    "\x02\x05" .. "Cursor Keys/PAD = Left & Right   " .. "\x02\x06" .. "Space/A Button = Jump   " .. "\x02\x03" .. "Pause/Tab/X Button = Pause      " .. "\x02\x04" .. "Alt/Y Button = Tune On/Off     " .. "\x02\x05" .. "S/B Button = High Scores        " .. "\x02\x05" .. "Back + Start Buttons = Quit".. TEXT_32 ..
     "\x02\x07" .. "Guide " .. "\x02\x05" .. "M" .. "\x02\x03" .. "i" .. "\x02\x02" .. "n" .. "\x02\x06" .. "e" .. "\x02\x04" .. "r" .. "\x02\x07" .. " Willy through 20 " .. "\x02\x02" .. "lethal " .. "\x02\x07" .. "caverns ...",
 }
 
